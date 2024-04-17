72905848e7ada69830295b98b10a30c11f3dcc90
Anirudh Subbarao <asubbarao450@gmail.com>
2024-04-16 20:16:14 -0500

    added required code for example functions with more than 12 console.log out statements

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,47 +15,81 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('Robert'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
-
+console.log(addNumbers(5, 7));
+console.log(addNumbers(21, 89));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(first, second, third) {
+  return first * second * third;
 }
 
+console.log(multiplyThree(3, 5, 6));
+console.log(multiplyThree(3, 7, 9));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  else {
+    return false;
+  }
+
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
+console.log(isPositive(1));
 
+console.log(isPositive(-3));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
 
+  if (array.length > 0) {
+    return array[(array.length - 1)];
+  }
+
+  else {
+    return undefined;
+  }
 }
+let array2 = [];
+console.log(getLast(array2));
+let array = [2, 3, 3, 45, 5]
+console.log(getLast(array));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
 
+  for (let x = 0; x < array.length - 1; x++) {
+
+    if (array[x] == value) {
+      return true;
+      break;
+    }
+
+  }
+
+  return false;
+
 }
+
+let samp = [3, 4, 5, 6, 7];
+console.log(find(2, samp));
+console.log(find(3, samp));
 
 // ----------------------
 // Stretch Goals
